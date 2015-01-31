f856d37a03b3210e3752d92cb7ff0e350e78801e
mirenva <mirenva@mail.ru>
2015-02-01 02:36:45 +0800

    абырвалг

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -5,11 +5,27 @@ import play.*;
 import play.mvc.*;
 import views.html.*;
 import play.data.*;
+import java.io.FileInputStream;
 import static play.data.Form.form;
+import java.util.*;
+import play.db.ebean.*;
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.hssf.util.CellReference;
+import org.apache.poi.poifs.filesystem.POIFSFileSystem;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.util.CellRangeAddress;
+import org.apache.poi.ss.util.CellRangeAddressBase;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.*;
+
 
 public class Application extends Controller {
 
-static Form<Task> taskForm = form(Task.class);
+  static Form<Task> taskForm = form(Task.class);
   
   public static Result index() {
     return redirect(routes.Application.tasks());
@@ -17,48 +33,119 @@ static Form<Task> taskForm = form(Task.class);
   
   public static Result tasks() {
     return ok(
-      views.html.index.render(Task.all(), taskForm)
+      views.html.index.render(Task.all(),taskForm)
     );
   }
-  
-  public static Result newTask() {
-    Form<Task> filledForm = taskForm.bindFromRequest();
-    if(filledForm.hasErrors()) {
-      return badRequest(
-        views.html.index.render(Task.all(), filledForm)
-      );
-    } else {
-      Task.create(filledForm.get());
-      return redirect(routes.Application.tasks());  
+
+  public static Result findTasks() {
+    DynamicForm requestData = Form.form().bindFromRequest();
+    List<Task> taskList = Task.find.where()
+        .ilike("groupNumber", "%" + requestData.get("groupNumber") + "%")
+        .ilike("day", "%" + requestData.get("day") + "%")
+        .ilike("hours", "%" + requestData.get("hours") + "%")
+        .ilike("lection", "%" + requestData.get("lection") + "%")
+        .ilike("teacher", "%" + requestData.get("teacher") + "%")
+        .ilike("room", "%" + requestData.get("room") + "%")
+    .findList();
+    return ok(
+            views.html.index.render(taskList.all(),requestData)
+    );
+  }
+
+  // разбираем файл Excel в БД
+  public static void main(String[] args) throws Exception {
+
+    org.apache.poi.poifs.filesystem.POIFSFileSystem fs =
+            new org.apache.poi.poifs.filesystem.POIFSFileSystem(new FileInputStream("schedule.xls"));
+    org.apache.poi.hssf.usermodel.HSSFWorkbook workbook =
+            new org.apache.poi.hssf.usermodel.HSSFWorkbook(fs);
+    org.apache.poi.hssf.usermodel.HSSFSheet sheet1 = workbook.getSheetAt(0);
+    org.apache.poi.hssf.usermodel.HSSFRow row;
+    org.apache.poi.hssf.usermodel.HSSFCell cell;
+
+    String[][] data = new String[240][60];
+    // разбираем файл Excel в массив
+    for (int i = 0; i < 240; i++) {
+      row = sheet1.getRow(i);
+      if (row != null) {
+        for (int j = 0; j < 60; j++) {
+          cell = row.getCell(j);
+          if (cell != null) {
+            switch (cell.getCellType()) {
+              case HSSFCell.CELL_TYPE_STRING:
+                data[i][j] = cell.getStringCellValue();
+                break;
+            }
+          }
+        }
+      }
+    }
+    // массив уже парсим в БД
+
+    // ищем, где начинается расписание
+    int z = 1;
+    int x = 1;
+    for (int i=0; i<100; i++) {
+      if (data[x][1] = "Дни") {
+        z = x;
+        break;
+      }
+    } // z - строка, где начинается "чистое" расписание
+
+    int y = 3;
+
+    while (true) {
+      String gN = data[x][y]; //
+
+      while (true) {
+        if (x > 200) break;
+
+        x = x + 1;
+        if (data[x][1] != null) {
+          String d = data[x][1];
+          while (true) {
+            if (data[x][2] != null) {
+              Task task = new Task();
+              task.groupNumber = gN;
+              task.day = d;
+              task.hours = data[x][2];
+              task.lection = data[x][y];
+              task.teacher = data[x][y + 1];
+              task.room = data[x][y + 2];
+              task.save();
+            }
+            else {
+              if (data[x][y] != null) {
+                Task task = new Task();
+                task.groupNumber = gN;
+                task.day = d;
+                task.hours = data[x][2];
+                task.lection = data[x][y];
+                task.teacher = data[x][y + 1];
+                task.room = data[x][y + 2];
+                task.save();
+              }
+            }
+            if (data[x + 1][1] != null) {
+              break;
+            } else {
+              x = x + 1;
+              if (x > 200) {
+                break;
+              }
+            }
+          }
+
+
+        }
+      }
+      y = y + 3;
+      x = z;
+      if (data[x][y] = "Часы") {
+        break;
+      }
     }
   }
-  
-  public static Result deleteTask(Long id) {
-    Task.delete(id);
-    return redirect(routes.Application.tasks());
+
+
   }
-	
-  public static Result editTaskForm(Long id) {
-	Task t  = Task.find.ref(id);
-	if (t==null) {
-		return notFound();
-	}
-	Form<Task> filledForm = taskForm.fill(t);
-	return ok(
-		views.html.editpage.render(Task.all(), filledForm)
-	);
-  }
-  
-  public static Result editTask(Long id) {
-	Form<Task> filledForm = taskForm.bindFromRequest();
-    if(filledForm.hasErrors()) {
-      return badRequest(
-        views.html.editpage.render(Task.all(), filledForm)
-      );
-    } else {
-      Task.edit(filledForm.get());
-      return redirect(routes.Application.tasks());  
-    }
-  }
-  
-}
--- app/models/Task.java
+++ app/models/Task.java
@@ -1,45 +1,58 @@
 package models;
 
 import java.util.*;
-
 import play.db.ebean.*;
 import play.data.validation.Constraints.*;
-
 import javax.persistence.*;
 
 
 @Entity
 public class Task extends Model {
 
-  @Id
-  public Long id;
+  public String groupNumber;
+  public String day;
+  public String hours;
+  public String lection;
+  public String teacher;
+  public String room;
   
-  @Required
-  public String name;
-  
-  @Required
-  public String h_number;
-  
-  @Required
-  public String m_number;
-  
-  public static Finder<Long,Task> find = new Finder(
-    Long.class, Task.class
+  public static Finder<String,Task> find = new Finder(
+    String.class, Task.class
   );
   
   public static List<Task> all() {
     return find.all();
   }
-  
-  public static void create(Task task) {
-    task.save();
-  }
-  
-  public static void delete(Long id) {
-    find.ref(id).delete();
-  }
-  
-  public static void edit(Task task) {
-    task.update();
-  }  
+
+
+//пусть сеттеры-геттеры останутся на всякий случай
+//
+//  public void setId(Long id) {
+//	this.id = id;
+//  }
+//
+//  public Long getId() {
+//    return id;
+//  }
+//
+//  public void setName(String name) {
+//	  this.name = name;
+//  }
+//  public String getName() {
+//    return name;
+//  }
+
+//  public void setHNumber(String number) {
+//	  this.hNumber = number;
+//  }
+//  public String getHNumber() {
+//    return hNumber;
+//  }
+//
+//  public void setMNumber(String number) {
+//	  this.mNumber = number;
+//  }
+//  public String getMNumber() {
+//    return mNumber;
+//  }
 }
