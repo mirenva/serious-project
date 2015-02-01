f90f3a6ea57edb824d47b9f92e6142271ecfde9f
mirenva <mirenva@mail.ru>
2015-02-01 12:35:07 +0800

    а может быть так

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -146,7 +146,7 @@ public class Application extends Controller {
         .ilike("room", "%" + requestData.get("room") + "%")
     .findList();
     return ok(
-            views.html.index.render(taskList.all(),requestData)
+            views.html.index.render(taskList,requestData)
     );
   }
   
